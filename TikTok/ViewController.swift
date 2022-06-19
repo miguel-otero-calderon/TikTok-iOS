2025e354f9f253d4ca07d6dcae1fcbd5ece36df6
Miguel Otero <miguel-otero-calderon@hotmail.com>
2022-06-18 21:29:07 -0500

    Setting up project

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Miguel on 18/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+        
+    }
+
+
+}
+
